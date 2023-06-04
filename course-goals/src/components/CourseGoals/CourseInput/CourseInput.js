9292e1f26e0a5b5e829e158227d743144116f4c4
bakour1 <bakour512@gmail.com>
2023-06-04 12:46:24 +0100

    76. Introducing Styled Components
    npm install' --save styled-components@latest

--- course-goals/src/components/CourseGoals/CourseInput/CourseInput.js
+++ course-goals/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,18 +3,18 @@ import React, { useState } from 'react';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
-const CourseInput = props => {
+const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     if (event.target.value.trim().length > 0) {
       setIsValid(true);
     }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
       setIsValid(false);

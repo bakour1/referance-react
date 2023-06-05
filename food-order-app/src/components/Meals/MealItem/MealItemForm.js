c85f6db1c4effe91e07d96d99422b5d7160db996
bakour1 <bakour512@gmail.com>
2023-06-05 20:18:04 +0100

    138. Working on the "Shopping Cart" Component

--- food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -5,14 +5,14 @@ const MealItemForm = (props) => {
   return (
     <form className={classes.form}>
       <Input
-        label="Amount"
+        label='Amount'
         input={{
-          id: 'amount_' + props.id, // this changed!
+          id: 'amount_' + props.id,
           type: 'number',
           min: '1',
           max: '5',
           step: '1',
-          defaultValue: '1'
+          defaultValue: '1',
         }}
       />
       <button>+ Add</button>

d9f2f1302fa12de5252d17da047a4aeb7309dc7e
bakour1 <bakour512@gmail.com>
2023-06-11 13:43:01 +0100

    272. Navigating between Pages with Links

--- react-router-app/src/pages/Home.js
+++ react-router-app/src/pages/Home.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
-const HomePage = () => {
-  return <div>HomePage</div>;
-};
+function HomePage() {
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        Go to <Link to="/products">the list of products</Link>.
+      </p>
+    </>
+  );
+}
 
 export default HomePage;
--- react-router-app/src/pages/Products.js
+++ react-router-app/src/pages/Products.js
@@ -1,7 +1,5 @@
-import React from 'react';
-
-const ProductsPage = () => {
-  return <div>ProductsPage</div>;
-};
+function ProductsPage() {
+  return <h1>The Products Page</h1>;
+}
 
 export default ProductsPage;

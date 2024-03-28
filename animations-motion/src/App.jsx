9990034c4f68fa45f2d33cb17f65e8c3ebac691f
bakour1 <81446233+bakour1@users.noreply.github.com>
2024-03-28 06:01:44 +0100

    save motion

--- animations-motion/src/App.jsx
+++ animations-motion/src/App.jsx
@@ -1,44 +1,15 @@
-import { useState } from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import WelcomePage from './pages/Welcome.jsx';
+import ChallengesPage from './pages/Challenges.jsx';
+
+const router = createBrowserRouter([
+  { path: '/', element: <WelcomePage /> },
+  { path: '/challenges', element: <ChallengesPage /> },
+]);
 
 function App() {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-  const [rotate, setRotate] = useState(0);
-
-  return (
-    <div id="demo">
-      <div id="box" />
-
-      <div id="inputs">
-        <p>
-          <label htmlFor="x">X</label>
-          <input
-            type="number"
-            id="x"
-            onChange={(event) => setX(+event.target.value)}
-          />
-        </p>
-
-        <p>
-          <label htmlFor="y">Y</label>
-          <input
-            type="number"
-            id="y"
-            onChange={(event) => setY(+event.target.value)}
-          />
-        </p>
-
-        <p>
-          <label htmlFor="rotate">Rotate</label>
-          <input
-            type="number"
-            id="rotate"
-            onChange={(event) => setRotate(+event.target.value)}
-          />
-        </p>
-      </div>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
